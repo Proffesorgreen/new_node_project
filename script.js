f7df26d76db8404d80d5609b48e4d44a3a5c3d7f
Proffesorgreen <legesehabtu@gmail.com>
2025-02-26 12:05:41 -0800

    task_2

---
+++ script.js
@@ -0,0 +1,8 @@
+import lodash from "lodash";
+
+const numbers = [1, 2, 3, 4, 5];
+const min_val = lodash.min(numbers);
+const max_val = lodash.max(numbers);
+
+console.log(min_val);
+console.log(max_val);
